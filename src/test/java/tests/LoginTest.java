71329408a73a1c6b8f48fbc1cd5929f1d809b0fd
tatyana <89168331085@mail.ru>
2026-04-26 13:58:52 +0300

    Add overloaded login(User user) method and fix LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -7,23 +7,22 @@ import user.UserFactory;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static user.UserFactory.standardUser;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void checkLogin() {
-        User user = UserFactory.standardUser();
         loginPage.open();
-        loginPage.login(user.getUsername(), user.getPassword());
+        loginPage.login(standardUser());
 
         assertEquals(productsPage.getTitle(), "Products");
     }
 
     @Test(dataProvider = "loginData")
-    public void checkIncorrectLogin() {
-        User user = UserFactory.standardUser();
+    public void checkIncorrectLogin(User user) {
         loginPage.open();
-        loginPage.login(user.getUsername(), user.getPassword());
+        loginPage.login(user);
 
         assertTrue(loginPage.isErrorMessageDisplayed(), "The error message fails to appear");
         assertEquals(loginPage.getErrorMessageText(), user.getErrorMessage(), "Error message doesn`t correspond");
